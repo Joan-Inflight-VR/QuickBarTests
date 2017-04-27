9cca3f3a3bd6b54b376935a2e92d27ed6be69825
Joan Mora InflightVR <joan@inflight-vr.com>
2017-04-27 17:23:49 +0200

    Initializing repo for QuickBarTests

---
+++ Assets/AngleDisplayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AngleDisplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponentInChildren<Text>().text = Camera.main.transform.eulerAngles.x.ToString();
+	}
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    bool mouseClick;
+    public Transform centralObject;
+    public float cameraRotateSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseClick = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseClick = false;
+        }
+
+        if (mouseClick)
+        {
+            transform.RotateAround(centralObject.position, centralObject.up, Input.GetAxis("Mouse X") * cameraRotateSpeed);
+            transform.RotateAround(centralObject.position, centralObject.right, Input.GetAxis("Mouse Y") * cameraRotateSpeed);
+        }
+    }
+}
